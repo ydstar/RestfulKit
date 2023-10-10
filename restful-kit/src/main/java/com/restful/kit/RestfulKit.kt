292f64d7577320bdcc8107a9d79b830d3cfaf1bb
信仰年轻 <hydznsqk@163.com>
2023-10-10 12:29:10 +0800

    动态代理

--- restful-kit/src/main/java/com/restful/kit/RestfulKit.kt
+++ restful-kit/src/main/java/com/restful/kit/RestfulKit.kt
@@ -18,10 +18,23 @@ import java.util.concurrent.ConcurrentHashMap
  *  2.这个方法的返回值必须是ICall<?>
  *  3.定义create方法,传入接口的class字节码对象得到具接口实现类,然后就调用方法,用了动态代理,当方法被调用的时候就调用了
  *  Proxy的invoke方法,在这个方法内解析请求方法的各种注解然后封装成Request对象,然后把request对象封装成ProxyCall对象
- *  4.这个ProxyCall对象就是ICall<?>的实现类对象,然后在invoke方法内返回,因为动态代理这个invoke要返回的值就是传入的接口中的被调用的方法的返回值
+ *  4.这个ProxyCall对象就是ICall<?>,的实现类对象然后在invoke方法内返回,因为动态代理这个invoke要返回的值就是传入的接口中的被调用的方法的返回值
  *   所以必须返回ICall<?>的实现类对象
  *  5.ProxyCall本身是真正的发起网络请求的类,因为ProxyCall对象是用Scheduler的newCall方法创建的,然后方法内是用的callFactory创建的
  *  6.这个callFactory是在外界ApiFactory类中创建RestfulKit的时候传递进来的
+ *
+ *
+ *  与静态代理不同的是,动态代理是通过反射在运行时生成代理对象,Java给我们提供了一个便捷的动态代理接口InvocationHandler,
+ *  源码最终调用的是Native方法去生成我们的代理对象
+ *
+ **动态代理原理及实现**
+ *  `Proxy.newProxyInstance`：⽤于动态创建代理对象，它需要传递三个参数：
+ *  `ClassLoader`：代理类需要加载的类加载器，⼀般直接使⽤`getClass().getClassLoader()`获取。
+ *  `RealSubjectInterfaces`：代理类需要实现的接⼝，⼀般使⽤`getClass().getInterfaces()`获取该对象的所有接⼝。
+ *  `InvocationHandler` 接⼝：动态代理类需要实现这个接⼝，它是代理对象拦截原始对象的处理者，在`invoke`回调⽅法中做拦截操作即可。
+ *
+ *  原理：动态代理其实是⾃动⽣成⼀个代理类的字节码，类名⼀般都是`Proxy$0`啥的，这个类会⾃动实现原始类实现的接⼝⽅法，然后再使⽤反射机制调⽤接⼝中的所有⽅法。
+ *  `Retrofit` 应⽤：`Retrofit `通过动态代理，为我们定义的请求接⼝都⽣成⼀个动态代理对象，实现请求。
  */
 class RestfulKit constructor(val baseUrl: String, callFactory: ICall.Factory) {
 
